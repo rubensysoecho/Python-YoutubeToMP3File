ce932f6d2f6ef96d8c8ca1ab49a88a784730a22c
rubensysoecho <rubenmartinezTRABALLOS@gmail.com>
2022-11-07 23:20:42 +0100

    turned download to a function

--- descargar_mp3.py
+++ descargar_mp3.py
@@ -1,30 +1,32 @@
 import os
 os.system('pip install pytube')
 
-from pytube import YouTube
 
+def downloadAudio(directoryOutput):
+    from pytube import YouTube
+    url = input("Youtube URL to download --> ")
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download(directoryOutput)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    print(yt.title + " downloaded correctly.")
+    
 output_directory = '.'
-
-print("1.- Default destination(where the py script is)")
-print("2.- Insert destination")
+print("1.- Default directory(where the py script is)")
+print("2.- Insert directory")
 option = int(input(""))
 
 if option == 1:
     possible = True
 elif option == 2:
     possible = True
-    output_directory = input("Destination --> ")
+    output_directory = input("Directory --> ")
 else:
     possible = False
 
 if possible:
-    url = input("Youtube URL to download --> ")
-    yt = YouTube(url)
-    video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download(output_directory)
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    print(yt.title + " descargado correctamente.")
+    downloadAudio(output_directory)
 else:
     print("Invalid option. Program finished.")
