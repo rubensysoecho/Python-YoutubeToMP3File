1bfe509fb903440411399e8071ccce6764bd6d21
Rubén <95583458+rubensysoecho@users.noreply.github.com>
2022-11-07 12:33:30 +0100

    upgrading functionality
    option to input file destination

--- descargar_mp3.py
+++ descargar_mp3.py
@@ -1,15 +1,30 @@
-from pytube import YouTube
 import os
+os.system('pip install pytube')
 
-url = input("URL --> ")
-yt = YouTube(url)
+from pytube import YouTube
 
-video = yt.streams.filter(only_audio=True).first()
+output_directory = '.'
 
-out_file = video.download('.')
+print("1.- Default destination(where the py script is)")
+print("2.- Insert destination")
+option = int(input(""))
 
-base, ext = os.path.splitext(out_file)
-new_file = base + '.mp3'
-os.rename(out_file, new_file)
+if option == 1:
+    possible = True
+elif option == 2:
+    possible = True
+    output_directory = input("Destination --> ")
+else:
+    possible = False
 
-print(yt.title + " descargado correctamente.")
+if possible:
+    url = input("Youtube URL to download --> ")
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download(output_directory)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    print(yt.title + " descargado correctamente.")
+else:
+    print("Invalid option. Program finished.")
